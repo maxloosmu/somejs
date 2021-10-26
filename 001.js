47d7fc39987eecec9f692d5cd0a8de828ddd540d
maxloosmu <maxloo@smu.edu.sg>
2021-10-26 20:17:55 +0800

    first commit

---
+++ 001.js
@@ -0,0 +1,21 @@
+var data = [1, 2, 3, 4, 5];
+var sum = 0;
+
+data.forEach(function(d){
+  sum += d;
+});
+
+console.log('Sum = ' + sum);
+
+function outer(bar){
+  this.bar = bar;
+  var that = this;
+  test = inner(2);
+  function inner(baz){
+    this.baz = baz * that.bar;
+    return this.baz;
+  }
+  return test;
+ }
+
+ console.log('baz * bar = ' + outer(2));
