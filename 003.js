8e813d18f70da93e563ce05acdf8bf48928dfe7b
maxloosmu <maxloo@smu.edu.sg>
2021-10-27 20:05:51 +0800

    3rd commit

---
+++ 003.js
@@ -0,0 +1,14 @@
+let anObject0 = {};
+console.log(anObject0.a);
+
+let anObject1 = {a:1, b:2, c:3};
+console.log(anObject1['a']);
+
+let anObject2 = {'a':1, 'b':2, 'c':3};
+console.log(anObject2.a);
+
+let anObject3 = {"a":1, "b":2, "c":3};
+console.log(anObject3['a']);
+
+let anObject4 = {"a key":"cat", "b key":"dog", "c key":"bird"};
+console.log(anObject4["c key"]);
