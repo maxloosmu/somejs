b80ee19f43dc1202a95e7a557c210dcc1e49d8cc
maxloosmu <maxloo@smu.edu.sg>
2021-10-30 23:23:21 +0800

    4th commit

---
+++ 004.js
@@ -0,0 +1,14 @@
+let selectPet = 0;
+switch (selectPet) {
+  case 1:
+    console.log("You got a cat!");
+    break;
+  case 2:
+    console.log("You got a dog!");
+    break;
+  case 3:
+    console.log("You got a bird!");
+    break;
+  default:
+    console.log("You got no pet!");
+}
