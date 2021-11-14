19345dd4339841a3a0494917321a7146fe75d6d0
maxloosmu <maxloo@smu.edu.sg>
2021-11-14 20:19:08 +0800

    7th commit

--- 003.js
+++ 003.js
@@ -12,3 +12,19 @@ console.log(anObject3['a']);
 
 let anObject4 = {"a key":"cat", "b key":"dog", "c key":"bird"};
 console.log(anObject4["c key"]);
+
+const newMap = new Map();
+newMap.set("cat", "cat house");
+newMap.set("dog", "dog house");
+newMap.set("bird", "bird house");
+console.log(newMap.size) // 3
+console.log(newMap.has('dog')); // true
+console.log(newMap.get('cat')); // cat house
+console.log(newMap.delete('tiger')); // false
+console.log(newMap.delete('dog')); // true
+console.log(newMap.size) // 2
+newMap.forEach((value, key) =>
+  console.log(`newMap[${key}] = ${value}`));
+
+const mapObject4 = new Map(Object.entries(anObject4));
+console.log(mapObject4.get('a key')); // cat
